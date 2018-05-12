0456ca8ed1af153d430b8186ac4c6aa5b28afd33
Colin S <csmail1290@gmail.com>
2018-05-12 10:31:22 -0700

    modules

--- ClassAct.js
+++ ClassAct.js
@@ -38,5 +38,5 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function() {
 
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT + "something bad about yourself");
 });
